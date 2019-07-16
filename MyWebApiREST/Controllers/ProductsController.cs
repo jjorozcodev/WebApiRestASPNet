b798737c024dc0a61fefebe3b77502f77486e6b2
JJ <jj.orozcolopez.13@gmail.com>
2019-07-15 23:01:11 -0600

    Added all methods REST
    Added GET, POST, PUT and DELETE methods to the WebAPI.
    Degrade bootstrap for visualization.

--- MyWebApiREST/Controllers/ProductsController.cs
+++ MyWebApiREST/Controllers/ProductsController.cs
@@ -12,6 +12,80 @@ namespace MyWebApiREST.Controllers
     {
         private NorthwindProducts dbContext = new NorthwindProducts();
 
+        // api/products
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return dbContext.Products.ToList();
+        }
+
+        // api/products/1
+        [HttpGet]
+        public Product Get(int id)
+        {
+            return dbContext.Products.FirstOrDefault(p => p.ProductID == id);
+        }
+
+        // api/products/create
+        [HttpPost]
+        public IHttpActionResult CreateProduct([FromBody]Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                dbContext.Products.Add(product);
+                dbContext.SaveChanges();
+                return Ok(product);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        // api/products/create
+        [HttpPut]
+        public IHttpActionResult UpdateProduct(int id, [FromBody]Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                bool ProductExist = dbContext.Products.Count(p => p.ProductID == id) > 0;
+
+                if(ProductExist)
+                {
+                    dbContext.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                    dbContext.SaveChanges();
+                    return Ok(product);
+                }
+                else
+                {
+                    return NotFound();
+                }
+                
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        // api/products/1
+        [HttpDelete]
+        public IHttpActionResult DeleteProduct(int id)
+        {
+            var prod = dbContext.Products.Find(id);
+
+            if(prod != null)
+            {
+                dbContext.Products.Remove(prod);
+                dbContext.SaveChanges();
+
+                return Ok(prod);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
 
 
     }
