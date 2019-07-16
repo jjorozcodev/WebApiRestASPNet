b5f8e761def3cc0a92f9e1a0cc6952ca2e13366f
JJ <jj.orozcolopez.13@gmail.com>
2019-07-15 21:53:27 -0600

    Adding the WebApi Controller
    Using WebApi 2 Controller Empty.
    Created the database context.

---
+++ MyWebApiREST/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MyWebApiREST.Models;
+
+namespace MyWebApiREST.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private NorthwindProducts dbContext = new NorthwindProducts();
+
+
+
+    }
+}
